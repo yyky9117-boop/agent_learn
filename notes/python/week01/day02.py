0f937a8fe81728a70e689d6607a282704ebd29ac
coconut <1505007609@qq.com>
2026-09-15 20:52:21 +0800

    docs: record day 2 learning review

--- notes/python/week01/day02.py
+++ notes/python/week01/day02.py
@@ -6,4 +6,4 @@ products = [
 
 # enumerate 同时获得序号和元素
 for rank, (item, value) in enumerate(products, start=1):
-    print("第",rank,"名", item, "显示",value,"元")
+    print("第",rank,"件商品", item, "价格",value,"元")
